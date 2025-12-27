f6dbe92070bd97bdcba2965d53772461f310f82c
Yahyakhaliman <yahyaindrayana35@gmail.com>
2025-12-27 12:57:02 +0700

    commit pertama

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use('/api', require('./routes/authRoute'));
 app.use('/api', require('./routes/homeRoute'));
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0',  () => {
     console.log('Server PlanToday running');
 });
