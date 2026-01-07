d5434415b8d46c1d027cf69a1fb28246d4ea3c49
Yahyakhaliman <yahyaindrayana35@gmail.com>
2026-01-07 09:10:10 +0700

    edit response

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1082,7 +1082,7 @@ const rekapCalonCustomerWA = async (req, res) => {
     if (!keyword) {
         return res.status(400).json({
         success: false,
-        message: "Parameter cc_nama wajib diisi untuk kirim WA (agar tidak semua data terkirim)",
+        message: "Tentukan Nama Customer (agar tidak semua data terkirim)",
         });
     }
 
